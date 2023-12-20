a93d53b70099b1a20c6018c849b31bc2d3070df6
isalng <122053624+isalng@users.noreply.github.com>
2023-12-20 11:26:45 -0800

    Player scene complete

---
+++ Player.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var Speed = 400.0
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -=1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -=1
+	if Input.is_action_pressed("move_down"):
+		velocity.y =1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * Speed
+	position += velocity * delta
+
+func _on_body_entered():
+	hide()
